897e28b28dd1f3c3b88e9ed5bb3606aee49b6522
nazim <nazim.bellahsene@gmail.com>
2016-02-11 23:29:13 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("button").onclick = function() {
+    alert("Thanks!");
+};
+    
+$("#circle").click(function(){
+    alert("I'm Here!");
+});
+
+$("#me").click(function(){
+    alert("Yep, it's me!")
+});
+
